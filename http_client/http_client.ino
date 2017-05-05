dfca1a6566b264412304fab861e47ca411446e9f
nevix <gal. nevis@gmail.com>
2017-05-05 22:57:07 +0300

    http client

---
+++ http_client/http_client.ino
@@ -0,0 +1,140 @@
+#include <SoftwareSerial.h>
+#include <SparkFunESP8266WiFi.h>
+
+const char mySSID[] = "oded";
+const char myPSK[] = "12345678";
+
+const char destServer[] = "example.com";
+const String httpRequest = "GET / HTTP/1.1\n"
+                           "Connection: close\n\n";
+void setup()
+{
+  Serial.begin(9600);
+  serialTrigger(F("Press any key to begin."));
+  initializeESP8266();
+  connectESP8266();
+  displayConnectInfo();
+}
+
+void loop(){
+  post();
+  delay(2000);
+}
+
+void initializeESP8266()
+{
+  int test = esp8266.begin();
+  if (test != true)
+  {
+    Serial.println(F("Error talking to ESP8266."));
+    errorLoop(test);
+  }
+  Serial.println(F("ESP8266 Shield Present"));
+}
+
+void connectESP8266()
+{
+  // The ESP8266 can be set to one of three modes:
+  //  1 - ESP8266_MODE_STA - Station only
+  //  2 - ESP8266_MODE_AP - Access point only
+  //  3 - ESP8266_MODE_STAAP - Station/AP combo
+  // Use esp8266.getMode() to check which mode it's in:
+  int retVal = esp8266.getMode();
+  if (retVal != ESP8266_MODE_STA)
+  { // If it's not in station mode.
+    // Use esp8266.setMode([mode]) to set it to a specified
+    // mode.
+    retVal = esp8266.setMode(ESP8266_MODE_STA);
+    if (retVal < 0)
+    {
+      Serial.println(F("Error setting mode."));
+      errorLoop(retVal);
+    }
+  }
+  Serial.println(F("Mode set to station"));
+
+  // esp8266.status() indicates the ESP8266's WiFi connect
+  // status.
+  // A return value of 1 indicates the device is already
+  // connected. 0 indicates disconnected. (Negative values
+  // equate to communication errors.)
+  retVal = esp8266.status();
+  if (retVal <= 0)
+  {
+    Serial.print(F("Connecting to "));
+    Serial.println(mySSID);
+    // esp8266.connect([ssid], [psk]) connects the ESP8266
+    // to a network.
+    // On success the connect function returns a value >0
+    // On fail, the function will either return:
+    //  -1: TIMEOUT - The library has a set 30s timeout
+    //  -3: FAIL - Couldn't connect to network.
+    retVal = esp8266.connect(mySSID, myPSK);
+    if (retVal < 0)
+    {
+      Serial.println(F("Error connecting"));
+      errorLoop(retVal);
+    }
+  }
+}
+
+void displayConnectInfo()
+{
+  char connectedSSID[24];
+  memset(connectedSSID, 0, 24);
+  // esp8266.getAP() can be used to check which AP the
+  // ESP8266 is connected to. It returns an error code.
+  // The connected AP is returned by reference as a parameter.
+  int retVal = esp8266.getAP(connectedSSID);
+  if (retVal > 0)
+  {
+    Serial.print(F("Connected to: "));
+    Serial.println(connectedSSID);
+  }
+
+  // esp8266.localIP returns an IPAddress variable with the
+  // ESP8266's current local IP address.
+  IPAddress myIP = esp8266.localIP();
+  Serial.print(F("My IP: ")); Serial.println(myIP);
+}
+
+void post()
+{
+
+  ESP8266Client client;
+
+  int retVal = client.connect(destServer, 80);
+  if (retVal <= 0)
+  {
+    Serial.println(F("Failed to connect to server."));
+    return;
+  }
+
+  client.print(httpRequest);
+
+  while (client.available())
+    Serial.write(client.read()); // read() gets the FIFO char
+
+  if (client.connected())
+    client.stop(); // stop() closes a TCP connection.
+}
+
+
+void errorLoop(int error)
+{
+  Serial.print(F("Error: ")); Serial.println(error);
+  Serial.println(F("Looping forever."));
+  for (;;)
+    ;
+}
+
+void serialTrigger(String message)
+{
+  Serial.println();
+  Serial.println(message);
+  Serial.println();
+  while (!Serial.available())
+    ;
+  while (Serial.available())
+    Serial.read();
+}
