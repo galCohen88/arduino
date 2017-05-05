005fb177e0bfcfcb912a2fbf17fd8cf9a96b93ba
nevix <gal. nevis@gmail.com>
2017-05-05 22:58:38 +0300

    remove comments

--- http_client/http_client.ino
+++ http_client/http_client.ino
@@ -34,16 +34,9 @@ void initializeESP8266()
 
 void connectESP8266()
 {
-  // The ESP8266 can be set to one of three modes:
-  //  1 - ESP8266_MODE_STA - Station only
-  //  2 - ESP8266_MODE_AP - Access point only
-  //  3 - ESP8266_MODE_STAAP - Station/AP combo
-  // Use esp8266.getMode() to check which mode it's in:
   int retVal = esp8266.getMode();
   if (retVal != ESP8266_MODE_STA)
-  { // If it's not in station mode.
-    // Use esp8266.setMode([mode]) to set it to a specified
-    // mode.
+  {
     retVal = esp8266.setMode(ESP8266_MODE_STA);
     if (retVal < 0)
     {
@@ -53,22 +46,11 @@ void connectESP8266()
   }
   Serial.println(F("Mode set to station"));
 
-  // esp8266.status() indicates the ESP8266's WiFi connect
-  // status.
-  // A return value of 1 indicates the device is already
-  // connected. 0 indicates disconnected. (Negative values
-  // equate to communication errors.)
   retVal = esp8266.status();
   if (retVal <= 0)
   {
     Serial.print(F("Connecting to "));
     Serial.println(mySSID);
-    // esp8266.connect([ssid], [psk]) connects the ESP8266
-    // to a network.
-    // On success the connect function returns a value >0
-    // On fail, the function will either return:
-    //  -1: TIMEOUT - The library has a set 30s timeout
-    //  -3: FAIL - Couldn't connect to network.
     retVal = esp8266.connect(mySSID, myPSK);
     if (retVal < 0)
     {
@@ -82,9 +64,6 @@ void displayConnectInfo()
 {
   char connectedSSID[24];
   memset(connectedSSID, 0, 24);
-  // esp8266.getAP() can be used to check which AP the
-  // ESP8266 is connected to. It returns an error code.
-  // The connected AP is returned by reference as a parameter.
   int retVal = esp8266.getAP(connectedSSID);
   if (retVal > 0)
   {
@@ -92,8 +71,6 @@ void displayConnectInfo()
     Serial.println(connectedSSID);
   }
 
-  // esp8266.localIP returns an IPAddress variable with the
-  // ESP8266's current local IP address.
   IPAddress myIP = esp8266.localIP();
   Serial.print(F("My IP: ")); Serial.println(myIP);
 }
@@ -113,10 +90,10 @@ void post()
   client.print(httpRequest);
 
   while (client.available())
-    Serial.write(client.read()); // read() gets the FIFO char
+    Serial.write(client.read());
 
   if (client.connected())
-    client.stop(); // stop() closes a TCP connection.
+    client.stop(); 
 }
 
 
